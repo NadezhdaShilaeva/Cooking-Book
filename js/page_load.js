7fe7b7318be54a5d26384738797a5c83141a8037
NadezhdaShilaeva <shilaeva5nadya@mail.ru>
2023-11-18 10:24:29 +0300

    refactor: my_recipes dirs

--- js/page_load.js
+++ js/page_load.js
@@ -1,17 +1,15 @@
 (function(){
-    window.addEventListener('load', outputInfoAboutDownload);
+    window.addEventListener('load', () => {
+        const pageEnd = performance.mark('pageEnd');
+        const pageloadtime = pageEnd.startTime / 1000;
+
+        let newElemLoad = document.createElement('p');
+        let newTextLoad = document.createTextNode(`Page load time is ${pageloadtime.toFixed(3)} seconds.`);
+        newElemLoad.appendChild(newTextLoad);
+        newElemLoad.style.padding = "10px 0px";
+        newElemLoad.setAttribute('align', 'center');
+
+        let footerElem = document.getElementsByClassName('footer-container__contacts')[0].parentNode;
+        footerElem.appendChild(newElemLoad);
+    });
 })();
-
-function outputInfoAboutDownload() {
-    const pageEnd = performance.mark('pageEnd');
-    const pageloadtime = pageEnd.startTime / 1000;
-
-    let newElemLoad = document.createElement('p');
-    let newTextLoad = document.createTextNode(`Page load time is ${pageloadtime.toFixed(3)} seconds.`);
-    newElemLoad.appendChild(newTextLoad);
-    newElemLoad.style.padding = "10px 0px";
-    newElemLoad.setAttribute('align', 'center');
-
-    let footerElem = document.getElementsByClassName('footer-container__contacts')[0].parentNode;
-    footerElem.appendChild(newElemLoad);
-}
